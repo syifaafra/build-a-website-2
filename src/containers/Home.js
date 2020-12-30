0526640f4360f47466f585ca47a82eefd9e7cd2e
unknown <afra.mumt@gmail.com>
2020-12-30 19:41:51 +0700

    downgraded tailwind

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,10 +5,32 @@ import React, { useState, useEffect, useMemo } from "react";
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
 
+import City from "../components/City";
+
 const weatherKey = `TODO`; // Your API Key here
 
-function Home() {
-    // TODO
+function Home() { <
+    div > < /div>;
+        // TODO
+    return (
+        // Container
+        <
+        div className = "flex flex-col h-screen bg-green-200" >
+        <
+        City cityName = { "Bogor" }
+        temp = { "10°C" }
+        color = { "bg-yellow-500" }
+        /> <
+        City cityName = { "Yogyakarta" }
+        temp = { "24°C" }
+        color = { "bg-red-500" }
+        /> <
+        City cityName = { "Aceh" }
+        temp = { "17°C" }
+        color = { "bg-blue-500" }
+        /> <
+        /div>
+    );
 }
 
 export default Home;
